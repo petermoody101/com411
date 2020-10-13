493e7b728d939e91328c76c0dcc2984a2c8b096a
repl.it user <replituser@example.com>
2020-10-13 10:20:45 +0000

    test

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,5 +1,6 @@
 print("Program Started")
 char1 = input("Please enter a standard character \n")
+#calculates the ascii value for input if a single character
 if len(char1) == 1:
   code1 = ord(char1)
   print ("The ascii code for {} is {}".format(char1, code1))
