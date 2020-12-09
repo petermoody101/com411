65831d8f11e8bc86dceaf16ca6635e6342067f74
repl.it user <replituser@example.com>
2020-12-09 14:29:50 +0000

    Updated human and robot classes with instance methods 2

--- OOP/human.py
+++ OOP/human.py
@@ -17,6 +17,24 @@ class Human:
   #defining an informal string method
   def __str__(self):
     return f"Human {self.name} is {self.age} years old."
+  #defining grow method
+  def grow(self):
+    self.age = self.age + 1
+  #defining eat method
+  def eat(self, amount):
+    #if statement to ensure energy does not exceed max energy
+    if self.energy = MAX_ENERGY:
+      print "Energy full, no need to eat"
+    else:
+      self.energy = self.energy + amount
+  #defining move method
+  def move(self, distance):
+    #if statement to ensure energy does not fall lower than 0
+    if self.energy != 0:
+      self.energy = self.energy - distance
+    else:
+      print("No energy left, cannot move")
+
 
 
 human = Human()
